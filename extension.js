9e487712bbb689f20d1e89f0c84ae0a4186ae6de
narsenico <not_amon-dev@yahoo.it>
2021-03-29 09:15:10 +0200

    Added skipFirstSelection option

--- extension.js
+++ extension.js
@@ -2,8 +2,9 @@
  * vscode-progressive-increment
  * https://github.com/narsenico/vscode-progressive-increment
  *
- * Copyright (c) 2019, Gianfranco Caldi.
- * Released under the MIT License.
+ * @author Gianfranco Caldi.
+ * @version 1.0.2
+ * @license MIT
  */
 const vscode = require('vscode');
 
@@ -12,69 +13,94 @@ const vscode = require('vscode');
  * Quindi se il primo numero trovato è 0, questo verrà portato a 0 + increment.
  * Per i numeri successivi non importa il loro valore attuale, verranno sempre incrementati
  * rispetto al numero precedente.
- * Se possibile cerca sempre di mantenere la lunghezza del numero originale nel caso fillando con 
+ * Se possibile cerca sempre di mantenere la lunghezza del numero originale nel caso fillando con
  * degli '0'.
  * Es: 0 0 0 => 1 2 3
  * Es: 00 00 00 => 01 02 03
  * Es: 0 10 12 => 1 02 03
- * 
- * @param {Number} increment valore numerico di incremento
+ *
+ * Se è attiva l'opzione skipFirstSelection il primo numero trovato nelle selezioni non viene incrementato,
+ * ma viene comunque considerato come valore iniziale per incrementare i successivi numeri.
+ * Es: 0 0 0 => 0 1 2
+ *
+ * @param {number} increment valore numerico di incremento
+ * @param {object} options opzioni
+ * - skipFirstSelection: il primo numero non viene incrementato
  * @returns {Promise|undefined} ritorna una promessa se ci sono delle selezioni
  * in cui eseguire l'opearazione, altirmenti undefined
  */
-function execIncrementBy(increment) {
+function execIncrementBy(increment, options = { skipFirstSelection: false }) {
     const editor = vscode.window.activeTextEditor;
     if (editor && editor.selections && editor.selections.length > 0) {
-        return editor.edit(editBuilder => {
-            const selections = editor.selections;
-            let selectionText, replacedText;
-            let refValue;
-            for (let ii = 0; ii < selections.length; ii++) {
-                selectionText = editor.document.getText(selections[ii]);
-                // cerco tutte le porzioni di stringa contenente valori numerici
-                // e li incremento considerando come valore iniziale il primo numero trovato
-                // cerco di mantenere 
-                replacedText = selectionText.replace(/\d+/g, nn => {
-                    if (refValue === undefined) {
-                        refValue = +nn;
-                    }
-                    let val = (refValue += increment).toString();
-                    // se il valore incrementato ha una lunghezza inferiore della stringa nn
-                    // presumo che nn sia preceduta da '0'
-                    if (val.length < nn.length) {
-                        // aggiungo tanti '0' davanti al valore
-                        val = Array(nn.length - val.length).fill('0').join('') + val;
-                    }
-                    return val;
-                });
+        return editor
+            .edit((editBuilder) => {
+                const selections = editor.selections;
+                let selectionText, replacedText;
+                let refValue;
+                for (let ii = 0; ii < selections.length; ii++) {
+                    selectionText = editor.document.getText(selections[ii]);
+                    // cerco tutte le porzioni di stringa contenente valori numerici
+                    // e li incremento considerando come valore iniziale il primo numero trovato
+                    // cerco di mantenere
+                    replacedText = selectionText.replace(/\d+/g, (nn) => {
+                        if (refValue === undefined) {
+                            refValue = +nn;
+                            // se devo skippare il primo valore esco subito
+                            if (options.skipFirstSelection) {
+                                return nn;
+                            }
+                        }
+                        let val = (refValue += increment).toString();
+                        // se il valore incrementato ha una lunghezza inferiore della stringa nn
+                        // presumo che nn sia preceduta da '0'
+                        if (val.length < nn.length) {
+                            // aggiungo tanti '0' davanti al valore
+                            val =
+                                Array(nn.length - val.length)
+                                    .fill('0')
+                                    .join('') + val;
+                        }
+                        return val;
+                    });
 
-                if (selectionText != replacedText) {
-                    editBuilder.replace(selections[ii], replacedText);
+                    if (selectionText != replacedText) {
+                        editBuilder.replace(selections[ii], replacedText);
+                    }
                 }
-            };
-        })/* .then(resp => {
+            }) /* .then(resp => {
             console.log('>>> Edit could be applied:', resp);
-        }) */.catch(err => {
-            console.error(err);
-        });
+        }) */
+            .catch((err) => {
+                console.error(err);
+            });
     }
+}
+
+function readOptions() {
+    const { skipFirstSelection } = vscode.workspace.getConfiguration(
+        'progressive'
+    );
+    return { skipFirstSelection };
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
     // implemento i comandi definiti nel package.json
-    // i comandi devono ritornare sempre altrimenti vscode non sa quando finiscono 
+    // i comandi devono ritornare sempre altrimenti vscode non sa quando finiscono
     // (e i test non funzionano)
-    context.subscriptions.push(vscode.commands.registerCommand('progressive.incrementBy1', function () {
-        return execIncrementBy(1);
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('progressive.incrementBy10', function () {
-        return execIncrementBy(10);
-    }));
+    context.subscriptions.push(
+        vscode.commands.registerCommand('progressive.incrementBy1', () =>
+            execIncrementBy(1, readOptions())
+        )
+    );
+    context.subscriptions.push(
+        vscode.commands.registerCommand('progressive.incrementBy10', () =>
+            execIncrementBy(10, readOptions())
+        )
+    );
 }
 exports.activate = activate;
 
-function deactivate() { }
+function deactivate() {}
 exports.deactivate = deactivate;

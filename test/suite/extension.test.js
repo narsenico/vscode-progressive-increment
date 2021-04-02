2f77ae171fcfd0121cfc131d69132c3b2c3ad3bf
narsenico <not_amon-dev@yahoo.it>
2021-04-02 15:35:57 +0200

    Fix test timeout

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -71,21 +71,83 @@ function normalizeText(text) {
 	return text ? text.replace(/\r\n/g, '\n') : '';
 }
 
-async function exec(fn) {
-    const rgTestExt = /\.test$/;
-    // cerco tutti i file .test nella cartella cases/
-    // per ogni file eseguo la funzione in parametro passando il percorso
-    //  del file di test e del corrispettivo contenente il risultato aspettato dopo l'incremento
-    const folder = path.join(__dirname, "cases");
-    const list = await fs.readdir(folder);
-    for (const testfile of list.filter(f => rgTestExt.test(f))) {
-		// if (!/test1_skipfirst/.test(testfile)) continue;
-        const expectedfile = path.join(folder, testfile.replace(rgTestExt, '.expected'));
-        await fn(path.join(folder, testfile), expectedfile);
-    }
-}
+// async function exec(fn) {
+//     const rgTestExt = /\.test$/;
+//     // cerco tutti i file .test nella cartella cases/
+//     // per ogni file eseguo la funzione in parametro passando il percorso
+//     //  del file di test e del corrispettivo contenente il risultato aspettato dopo l'incremento
+//     const folder = path.join(__dirname, "cases");
+//     const list = await fs.readdir(folder);
+//     for (const testfile of list.filter(f => rgTestExt.test(f))) {
+// 		// if (!/test1_skipfirst/.test(testfile)) continue;
+//         const expectedfile = path.join(folder, testfile.replace(rgTestExt, '.expected'));
+//         await fn(path.join(folder, testfile), expectedfile);
+//     }
+// }
 
-suite('Extension Tests', function () {
-    test('Select all and increments by 1', async () => await exec(selectAllAndIncrements));
-    test('Split into selections and increments by 1', async () => await exec(splitSelectionAndIncrements));
+// suite('Extension Tests', function () {
+//     test('Select all and increments by 1', async () =>
+//         await exec(selectAllAndIncrements));
+//     test('Split into selections and increments by 1', async () =>
+//         await exec(splitSelectionAndIncrements));
+// });
+
+suite('Select all', async function () {
+    const folder = path.join(__dirname, 'cases');
+
+    test('Increment all by 1: test1.test', async () =>
+        await selectAllAndIncrements(
+            path.join(folder, 'test1.test'),
+            path.join(folder, 'test1.expected')
+        ));
+    test('Increment all by 1: test2.test', async () =>
+        await selectAllAndIncrements(
+            path.join(folder, 'test2.test'),
+            path.join(folder, 'test2.expected')
+        ));
+    test('Increment all by 1: test3.test', async () =>
+        await selectAllAndIncrements(
+            path.join(folder, 'test3.test'),
+            path.join(folder, 'test3.expected')
+        ));
+    test('Increment all by 1: test4.test', async () =>
+        await selectAllAndIncrements(
+            path.join(folder, 'test4.test'),
+            path.join(folder, 'test4.expected')
+        ));
+    test('Increment all by 1: test1_skipfirst.test', async () =>
+        await selectAllAndIncrements(
+            path.join(folder, 'test1_skipfirst.test'),
+            path.join(folder, 'test1_skipfirst.expected')
+        ));
+});
+
+suite('Split selections', async function () {
+    const folder = path.join(__dirname, 'cases');
+
+    test('Increment all by 1: test1.test', async () =>
+        await splitSelectionAndIncrements(
+            path.join(folder, 'test1.test'),
+            path.join(folder, 'test1.expected')
+        ));
+    test('Increment all by 1: test2.test', async () =>
+        await splitSelectionAndIncrements(
+            path.join(folder, 'test2.test'),
+            path.join(folder, 'test2.expected')
+        ));
+    test('Increment all by 1: test3.test', async () =>
+        await splitSelectionAndIncrements(
+            path.join(folder, 'test3.test'),
+            path.join(folder, 'test3.expected')
+        ));
+    test('Increment all by 1: test4.test', async () =>
+        await splitSelectionAndIncrements(
+            path.join(folder, 'test4.test'),
+            path.join(folder, 'test4.expected')
+        ));
+    test('Increment all by 1: test1_skipfirst.test', async () =>
+        await splitSelectionAndIncrements(
+            path.join(folder, 'test1_skipfirst.test'),
+            path.join(folder, 'test1_skipfirst.expected')
+        ));
 });
